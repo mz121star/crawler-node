2036bbd8492235b40df2bf8b2ed44c54d3583aac
mz121star <mz121star@gmail.com>
2013-11-26 17:09:49 +0800

    1版本 横向增加不好用

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,57 @@
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-    var self=this
+    var self = this
     self.firstName = ko.observable("Bert");
     self.lastName = ko.observable("Bertington");
-    self.fullName = ko.computed(function() {
+    self.fullName = ko.computed(function () {
         return self.firstName() + " " + self.lastName();
     }, self);
-    self.capitalizeLastName = function() {
+    self.capitalizeLastName = function () {
         var currentVal = self.lastName();        // Read the current value
         self.lastName(currentVal.toUpperCase()); // Write back a modified value
     };
-    self.addText=function(){
-        self.keyVals.push({key:ko.observable("Title"),value:ko.observable("/<title>(.*)<\/title>/")}) ;
+    self.addNewText = function () {
+      /*  self.keyVals.push({
+            key: ko.observable("Title"),
+            value: ko.observableArray([
+                ko.observable("/text/")
+            ])
+        });*/
+        self.keyVals.push({
+            key: ko.observable("Title"),
+            values: ko.observableArray([
+                ko.observable("/text/")
+            ])
+        });
     }
-    self.keyVals=ko.observableArray([
+    self.addNextText = function (kv) {
+        kv.values.push("/new/");
+       /* self.keyVals()[0].values.push(("/new/"))*/
+    }
+    self.keyVals = ko.observableArray([
         {
-            key:ko.observable("Title")
-           ,value:ko.observable("/<title>(.*)<\/title>/")
+            key: ko.observable("Title"),
+            values: ko.observableArray([
+                ko.observable("/text/")
+            ])
         }
     ]);
-    self.remove = function(kv) {
+    self.remove = function (kv) {
         self.keyVals.remove(kv)
     }
-    self.syntax = ko.computed(function() {
+    self.syntax = ko.computed(function () {
+        console.log("over")
         var result = "";
-        for (var i = 0; i < self.keyVals().length; i++)
-            result += self.keyVals()[i].key()  + "="+ self.keyVals()[i].value()+","
+        for (var i = 0; i < self.keyVals().length; i++) {
+            result += self.keyVals()[i].key() + "=";
+
+           /*result += self.keyVals()[i].values().join("=>") + ",";*/
+            for (var k = 0; k < self.keyVals()[i].values().length; k++) {
+                result += self.keyVals()[i].values()[k]()+"=>";
+            }
+            result+=","
+
+        }
         return result;
     });
 
